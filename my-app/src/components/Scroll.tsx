14455af32cc6782f3a5b1797b7bcdcc90a0a6543
Abkels <abrahamk29@gmail.com>
2023-10-28 22:33:59 +0100

    changes

--- my-app/src/components/Scroll.tsx
+++ my-app/src/components/Scroll.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const Scroll: React.FC = (props) => {
+type Props = {
+    children?: JSX.Element
+}
+
+const Scroll = (props: Props) => {
   return (
     <div style={{ overflowY: 'scroll', border: '5px solid black', height: '500px' }}>
       {props.children}
